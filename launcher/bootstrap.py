18731b1c100175a6824fa237a4f84ed43ec4ee39
olisolomons <oli.solomons@gmail.com>
2022-01-02 20:52:00 +0000

    Change to cache dir to avoid space in conda path

--- launcher/bootstrap.py
+++ launcher/bootstrap.py
@@ -6,13 +6,13 @@ from subprocess import run
 import sys
 
 rent_manager_dirs = appdirs.AppDirs('RentManager')
-user_data = Path(rent_manager_dirs.user_data_dir)
+user_cache = Path(rent_manager_dirs.user_cache_dir)
 script_dir = Path(__file__).parent
 
-bootstrap_complete_marker = user_data / 'bootstrap_complete'
-conda_dir = user_data / 'python' / 'miniconda'
-conda_venv_dir = user_data / 'python' / 'conda_venv'
-venv_dir = user_data / 'python' / 'venv'
+bootstrap_complete_marker = user_cache / 'bootstrap_complete'
+conda_dir = user_cache / 'python' / 'miniconda'
+conda_venv_dir = user_cache / 'python' / 'conda_venv'
+venv_dir = user_cache / 'python' / 'venv'
 requirements = script_dir / 'launcher_requirements.txt'
 
 
@@ -20,13 +20,10 @@ def bootstrap():
     if bootstrap_complete_marker.exists():
         return
 
-    shutil.rmtree(user_data / 'python', ignore_errors=True)
-    (user_data / 'python').mkdir(parents=True)
+    shutil.rmtree(user_cache / 'python', ignore_errors=True)
+    (user_cache / 'python').mkdir(parents=True)
 
-    print(f"{(script_dir / 'miniconda.sh')=}; {(script_dir / 'miniconda.sh').is_file()=}")
-    res = run(['/usr/bin/env', 'sh', str(script_dir / 'miniconda.sh'), '-b', '-p', str(conda_dir)])
-    print(res.returncode)
-    res.check_returncode()
+    run(['/usr/bin/env', 'sh', str(script_dir / 'miniconda.sh'), '-b', '-p', str(conda_dir)], check=True)
 
     run([conda_dir / 'bin' / 'conda', 'create', '-p', conda_venv_dir, 'python=3.9', '--yes'], check=True)
 

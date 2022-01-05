6ba814600f38fd18746866b25bdb650a6737932e
olisolomons <oli.solomons@gmail.com>
2022-01-05 15:22:56 +0000

    Boostrap windows fix

--- launcher/bootstrap.py
+++ launcher/bootstrap.py
@@ -5,14 +5,27 @@ import subprocess
 from subprocess import run
 import sys
 
+is_windows = sys.platform.startswith('win')
+
 rent_manager_dirs = appdirs.AppDirs('RentManager')
 user_cache = Path(rent_manager_dirs.user_cache_dir)
 script_dir = Path(__file__).parent
 
 bootstrap_complete_marker = user_cache / 'bootstrap_complete'
+
 conda_dir = user_cache / 'python' / 'miniconda'
 conda_venv_dir = user_cache / 'python' / 'conda_venv'
 venv_dir = user_cache / 'python' / 'venv'
+
+if is_windows:
+    conda_exec = conda_dir / '_conda.exe'
+    conda_venv_dir_python = conda_venv_dir / 'python.exe'
+    venv_dir_python = venv_dir / 'Scripts' / 'python.exe'
+else:
+    conda_exec = conda_dir / 'bin' / 'conda'
+    conda_venv_dir_python = conda_venv_dir / 'bin' / 'python'
+    venv_dir_python = venv_dir / 'bin' / 'python'
+
 requirements = script_dir / 'launcher_requirements.txt'
 
 
@@ -23,17 +36,17 @@ def bootstrap():
     shutil.rmtree(user_cache / 'python', ignore_errors=True)
     (user_cache / 'python').mkdir(parents=True)
 
-    if sys.platform.startswith('win'):
+    if is_windows:
         run([script_dir / 'miniconda.exe', '/InstallationType=JustMe', 'RegisterPython=0', '/S',
              f'/D={conda_dir}'], check=True)
     else:
         run(['/usr/bin/env', 'sh', script_dir / 'miniconda.sh', '-b', '-p', conda_dir], check=True)
 
-    run([conda_dir / 'bin' / 'conda', 'create', '-p', conda_venv_dir, 'python=3.9', '--yes'], check=True)
+    run([conda_exec, 'create', '-p', conda_venv_dir, 'python=3.9', '--yes'], check=True)
 
     venv_dir.mkdir(parents=True)
-    run([conda_venv_dir / 'bin' / 'python', '-m', 'venv', venv_dir], check=True)
-    run([venv_dir / 'bin' / 'python', '-m', 'pip', 'install', '-r', requirements], check=True)
+    run([conda_venv_dir_python, '-m', 'venv', venv_dir], check=True)
+    run([venv_dir_python, '-m', 'pip', 'install', '-r', requirements], check=True)
 
     bootstrap_complete_marker.touch()
 

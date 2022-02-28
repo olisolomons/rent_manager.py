468bed703d44bb56c243ccebe9cd9acad644e192
olisolomons <oli.solomons@gmail.com>
2022-02-28 00:55:06 +0000

    Line wrapping in report generation

--- src/report_generator.py
+++ src/report_generator.py
@@ -1,5 +1,9 @@
+import itertools
+
 import abc
+import dataclass_json
 import operator
+import re
 import typing
 from datetime import date
 
@@ -9,6 +13,7 @@ from rent_manager.state.rent_payment import RentPayment
 from typing import Sequence
 
 from fpdf import FPDF, HTMLMixin
+from xml.sax.saxutils import escape
 
 from rent_manager.state.rent_manager_state import RentManagerState, RentCalculations, RentManagerMainState
 
@@ -35,6 +40,9 @@ class PDFGenerator:
     def put(self, html_fragment):
         self.html_buffer.append(html_fragment)
 
+    def text(self, text):
+        self.html_buffer.append(escape(text))
+
     def tag(self, tag_name: str, **attributes: str):
         return AutoClosingTag(self, tag_name, **attributes)
 
@@ -54,20 +62,69 @@ class PDFGenerator:
                     for header, width in zip(headers, widths):
                         with self.tag('th', width=f'{width}%'):
                             if isinstance(header, str):
-                                self.put(header)
+                                self.text(header)
                             else:
-                                self.put(header[0])
+                                self.text(header[0])
             with self.tag('tbody'):
-                # TODO: wrap long lines into multiple rows
                 for i, row in enumerate(data):
-                    with self.tag('tr', bgcolor='#FFFFFF' if i % 2 == 0 else '#e9eef5'):
-                        for cell in row:
-                            with self.tag('td'):
-                                self.put(cell)
+                    wrapped_row = [self._wrap(cell, width) for cell, width in zip(row, widths)]
+                    # use matrix transpose to go from list of (lists of lines per cell) => list (list of sub-rows)
+                    wrapped_sub_rows = itertools.zip_longest(*wrapped_row, fillvalue=' ')
+                    color = '#FFFFFF' if i % 2 == 0 else '#e9eef5'
+                    for sub_row in wrapped_sub_rows:
+                        with self.tag('tr', bgcolor=color):
+                            for cell in sub_row:
+                                with self.tag('td'):
+                                    self.text(cell)
 
     def output(self, path):
         self.pdf.write_html(''.join(self.html_buffer))
         self.pdf.output(path)
+
+    def _wrap(self, cell: str, width_percent: int):
+        input_lines = cell.split('\n')
+        if len(input_lines) > 1:
+            return sum((self._wrap(line, width_percent) for line in input_lines), [])  # concatenate the line arrays
+
+        cell_width_px = self.pdf.epw * width_percent / 100 - 5
+
+        # separate by whitespace:
+        # \s* : capture any whitespace at the beginning of `cell`
+        # \S+ : capture the word (non-whitespace characters)
+        # (\s*) : capture whitespace at the end, separately so it can be added back as needed
+        words = re.findall(r'(\s*\S+)(\s*)', cell)
+        lines = []
+        line = ''
+        previous_whitespace = ''
+        i = 0
+        while i < len(words):  # using while loop so `i` can be modified during iteration
+            word, trailing_whitespace = words[i]
+
+            if self.pdf.get_string_width(word) > cell_width_px:
+                sub_word = ''
+                j = 0
+                for j, letter in enumerate(word):
+                    sub_word += letter
+                    if self.pdf.get_string_width(line + sub_word) > cell_width_px:
+                        break
+                lines.append(line + previous_whitespace + sub_word[:-1])
+                line = ''
+                previous_whitespace = ''
+
+                words[i] = word[j:], trailing_whitespace
+            else:
+                if self.pdf.get_string_width(line + word) > cell_width_px:
+                    lines.append(line)
+                    line = ''
+                    previous_whitespace = ''
+                line += previous_whitespace + word
+                previous_whitespace = trailing_whitespace
+
+                i += 1
+
+        if line:
+            lines.append(line)
+        return lines
 
 
 class AutoClosingTag:
@@ -135,7 +192,8 @@ def generate_report(data: RentManagerState, calculations: RentCalculations, expo
             color = 'AA0000'
         rent_for_months.append([
             month.strftime(month_format), format_currency(paid),
-            f'<font color="#{color}">' + format_currency(month_arrears) + '</font>'
+            format_currency(month_arrears)
+            # f'<font color="#{color}">' + format_currency(month_arrears) + '</font>'
         ])
     pdf.put_table(
         ['Month', 'Amount Paid', 'Arrears for month'],
@@ -231,4 +289,7 @@ def generate_report(data: RentManagerState, calculations: RentCalculations, expo
 
 
 if __name__ == '__main__':
-    hello_world()
+    data = '{"rent_manager_main_state": {"rent_payments": [{"amount": 1000, "received_on": "2022-02-25", "for_month": "2021-02-01"}, {"amount": 500, "received_on": "2022-02-25", "for_month": "2021-03-01"}, {"amount": 1100, "received_on": "2022-02-25", "for_month": "2021-04-01"}, {"amount": 1000, "received_on": "2022-02-27", "for_month": "2021-05-01"}], "other_transactions": [{"reason": 3, "amount": 2600, "comment": "", "_date": "2022-02-27"}, {"reason": 4, "amount": 130, "comment": "Delayed because of long reasons that take up a lot of space, forcing a multi-line comment", "_date": "2022-02-27"}, {"reason": 4, "amount": 50, "comment": "For month 05/2021", "_date": "2022-02-27"}, {"reason": 3, "amount": 820, "comment": "", "_date": "2022-02-27"}, {"reason": 4, "amount": 0, "comment": "AAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB", "_date": "2022-02-27"}, {"reason": 2, "amount": 0, "comment": "thisisareallylongsentencethathasnospacesinordertotestmytextwrappingfunctionalityyoumightthingthesentencewouldendherebutitactuallydoesntbecauseineedtokeepramblingtofillspace", "_date": "2022-02-27"}, {"reason": 2, "amount": 0, "comment": "And short + looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong g", "_date": "2022-02-27"}]}, "rent_arrangement_data": {"start_date": "2021-02-26", "monthly_rent": 1000, "agents_fee": 5.0}}'
+    data = dataclass_json.loads(data, RentManagerState)
+    calc = RentCalculations.from_rent_manager_state(data)
+    generate_report(data, calc, 'test.pdf')
